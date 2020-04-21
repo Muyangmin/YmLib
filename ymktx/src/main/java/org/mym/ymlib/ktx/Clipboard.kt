36d90841d5f2048762607d037cec1a5d509db415
Muyangmin <muyangmin@foxmail.com>
2020-04-21 18:41:58 +0800

    Add new extension function for clipboard usage.

---
+++ ymktx/src/main/java/org/mym/ymlib/ktx/Clipboard.kt
@@ -0,0 +1,25 @@
+package org.mym.ymlib.ktx
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
+
+/**
+ * 将数据复制到系统剪贴板中。
+ *
+ * @param[toastText] 复制成功后的提示文本，如果为空则不会弹出 toast。
+ * @param[clipDataBuilder] 构造待复制的数据。推荐使用 [ClipData.newPlainText] 系列方法。
+ *
+ * @since 0.7.0
+ */
+fun Context.copyToClipboard(
+    toastText: String? = getString(R.string.ymktx_msg_content_copied),
+    clipDataBuilder: () -> ClipData
+) {
+    val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    clipboard.primaryClip = clipDataBuilder()
+    if (!toastText.isNullOrEmpty()) {
+        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show()
+    }
+}
